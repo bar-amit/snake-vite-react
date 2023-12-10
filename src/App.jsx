7d7ab448085d2396f4059cb34f6129dc551fcb81
Bar Amit <bar.amit@outlook.com>
2023-12-10 23:19:51 +0200

    feat: random food

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 let cell_types = {empty: 0, snake: 1, food: 2};
@@ -6,15 +6,27 @@ let board_size = 10;
 let init_board = Array.from(Array(board_size), row=>Array.from(Array(board_size), cell => cell_types['empty']));
 
 function drawCell(cell, i){
-  return <div className={`cell_${cell}`} key={i}></div>;
+  return <div className={`cell cell_${cell}`} key={i}></div>;
 }
 
 function drawBoard(board){
   return board.map((row, i)=><div className='row' key={i}>{row.map(drawCell)}</div>);
 }
 
+function randomFood(board){
+  let empty_cells = board.reduce((acc, row, i)=>{
+    row.forEach((cell, j)=>{ if(cell==cell_types['empty']) acc.push([i, j]) });
+    return acc;
+  }, []);
+  let random = Math.floor(Math.random()*empty_cells.length);
+  let food = empty_cells[random];
+  board[food[0]][food[1]] = cell_types['food'];
+  console.log(food);
+  return board;
+}
+
 function App() {
-  const [board, setBoard] = useState(init_board);
+  const [board, setBoard] = useState(randomFood(init_board));
 
   return (
       <div className='container'>
