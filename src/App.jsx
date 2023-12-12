3a8f8935fbac89f9dd1dc832504ae53bea069aa7
Bar Amit <bar.amit@outlook.com>
2023-12-12 23:19:41 +0200

    add game functions (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import './App.css'
 
 let cell_types = {empty: 0, snake: 1, food: 2};
 let board_size = 10;
 let init_board = Array.from(Array(board_size), row=>Array.from(Array(board_size), cell => cell_types['empty']));
+let init_snake = [[0, 0], [0, 1], [0, 2]];
+let interval_delta = 900;
 
-function drawCell(cell, i){
+function draw_cell(cell, i){
   return <div className={`cell cell_${cell}`} key={i}></div>;
 }
 
-function drawBoard(board){
-  return board.map((row, i)=><div className='row' key={i}>{row.map(drawCell)}</div>);
+function draw_board(board){
+  return board.map((row, i)=><div className='row' key={i}>{row.map(draw_cell)}</div>);
 }
 
-function randomFood(board){
+function draw_snake({board, snake}){
+  console.log('snake',snake)
+  for(let c of snake){
+    console.log(c)
+    board[c[0]][c[1]] = cell_types['snake'];
+  }
+  return board;
+}
+
+function random_food(board){
   let empty_cells = board.reduce((acc, row, i)=>{
     row.forEach((cell, j)=>{ if(cell==cell_types['empty']) acc.push([i, j]) });
     return acc;
@@ -25,15 +36,54 @@ function randomFood(board){
   return board;
 }
 
+function move_snake(){
+  board = board.map(cell=>
+    cell==cell_types['snake']?cell_types['empty']:cell);
+  let new_cell = snake[snake.length-1];
+  switch(direction){
+    case 'left':
+      new_cell[1]--;
+      break;
+    case 'right':
+      new_cell[1]++;
+      break;
+    case 'up':
+      new_cell[0]--;
+      break;
+    case 'down':
+      new_cell[0]++;
+      break;
+  }
+  snake.shift();
+  snake.push(new_cell);
+  return draw_snake({board, snake});
+}
+
+let board = init_board;
+let snake = init_snake;
+let direction = 'right';
+let game_interval;
+
 function App() {
-  const [board, setBoard] = useState(randomFood(init_board));
+  const [game_board, set_game_board] = useState(draw_board(board));
+
+  function handle_start(){
+    board = random_food(draw_snake({board, snake}));
+    console.log(board)
+    set_game_board(draw_board(board));
+    game_interval = setInterval(()=>{
+      board = move_snake();
+      set_game_board(draw_board(board));
+    }, interval_delta);
+  }
 
   return (
+    <>
       <div className='container'>
-        {
-          drawBoard(board)
-        }
+        {game_board}
       </div>
+      <button type="button" onClick={handle_start}>Start</button>
+    </>
   )
 }
 
