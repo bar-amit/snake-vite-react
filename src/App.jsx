032f2a0656e9ab4e8a58f1bb2138ca148edf4b28
Bar Amit <bar.amit@outlook.com>
2023-12-13 21:50:52 +0200

    fix snake logic and refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from 'react'
 import './App.css'
 
-let cell_types = {empty: 0, snake: 1, food: 2};
+let cell_types = {empty: '0', snake: '1', food: '2'};
 let board_size = 10;
 let init_board = Array.from(Array(board_size), row=>Array.from(Array(board_size), cell => cell_types['empty']));
 let init_snake = [[0, 0], [0, 1], [0, 2]];
@@ -11,20 +11,18 @@ function draw_cell(cell, i){
   return <div className={`cell cell_${cell}`} key={i}></div>;
 }
 
-function draw_board(board){
+function draw_board(){
+  // console.log(board[0])
   return board.map((row, i)=><div className='row' key={i}>{row.map(draw_cell)}</div>);
 }
 
-function draw_snake({board, snake}){
-  console.log('snake',snake)
+function draw_snake(){
   for(let c of snake){
-    console.log(c)
     board[c[0]][c[1]] = cell_types['snake'];
   }
-  return board;
 }
 
-function random_food(board){
+function random_food(){
   let empty_cells = board.reduce((acc, row, i)=>{
     row.forEach((cell, j)=>{ if(cell==cell_types['empty']) acc.push([i, j]) });
     return acc;
@@ -32,14 +30,16 @@ function random_food(board){
   let random = Math.floor(Math.random()*empty_cells.length);
   let food = empty_cells[random];
   board[food[0]][food[1]] = cell_types['food'];
-  console.log(food);
-  return board;
+}
+
+function remove_snake(){
+  board = board.map(row=>row.map(cell=>cell==cell_types['snake'] ? cell_types['empty'] : cell));
 }
 
 function move_snake(){
-  board = board.map(cell=>
-    cell==cell_types['snake']?cell_types['empty']:cell);
-  let new_cell = snake[snake.length-1];
+  console.log(direction)
+  remove_snake();
+  let new_cell = Array.from(snake[snake.length-1]);
   switch(direction){
     case 'left':
       new_cell[1]--;
@@ -56,7 +56,25 @@ function move_snake(){
   }
   snake.shift();
   snake.push(new_cell);
-  return draw_snake({board, snake});
+  draw_snake();
+}
+
+function handle_arrow(e){
+  console.log(e.key)
+  switch(e.key){
+    case 'ArrowUp':
+      direction = 'up'
+      break;
+    case 'ArrowDown':
+      direction = 'down';
+      break;
+    case 'ArrowLeft':
+      direction = 'left';
+      break;
+    case 'ArrowRight':
+      direction = 'right';
+      break;
+  }
 }
 
 let board = init_board;
@@ -65,17 +83,22 @@ let direction = 'right';
 let game_interval;
 
 function App() {
-  const [game_board, set_game_board] = useState(draw_board(board));
+  const [game_board, set_game_board] = useState(draw_board());
 
-  function handle_start(){
-    board = random_food(draw_snake({board, snake}));
-    console.log(board)
-    set_game_board(draw_board(board));
+  function handle_start(e){
+    e.currentTarget.disabled = true;
+    draw_snake();
+    random_food();
+    set_game_board(draw_board());
     game_interval = setInterval(()=>{
-      board = move_snake();
-      set_game_board(draw_board(board));
+      move_snake();
+      set_game_board(draw_board());
     }, interval_delta);
   }
+
+  useEffect(()=>{
+    window.addEventListener('keydown', handle_arrow);
+  }, []);
 
   return (
     <>
