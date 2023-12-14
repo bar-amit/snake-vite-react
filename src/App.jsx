c2e99fdd2be641fe543f965d4c05c1febf9de036
Bar Amit <bar.amit@outlook.com>
2023-12-14 23:04:23 +0200

    add game logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 let cell_types = {empty: '0', snake: '1', food: '2'};
 let board_size = 10;
 let init_board = Array.from(Array(board_size), row=>Array.from(Array(board_size), cell => cell_types['empty']));
 let init_snake = [[0, 0], [0, 1], [0, 2]];
-let interval_delta = 900;
+let interval_delta = 500;
 
 function draw_cell(cell, i){
   return <div className={`cell cell_${cell}`} key={i}></div>;
 }
 
 function draw_board(){
-  // console.log(board[0])
   return board.map((row, i)=><div className='row' key={i}>{row.map(draw_cell)}</div>);
 }
 
@@ -36,8 +35,14 @@ function remove_snake(){
   board = board.map(row=>row.map(cell=>cell==cell_types['snake'] ? cell_types['empty'] : cell));
 }
 
+function game_over(){
+  board = init_board;
+  snake = init_snake;
+  lost = true;
+  clearInterval(game_interval);
+}
+
 function move_snake(){
-  console.log(direction)
   remove_snake();
   let new_cell = Array.from(snake[snake.length-1]);
   switch(direction){
@@ -54,13 +59,27 @@ function move_snake(){
       new_cell[0]++;
       break;
   }
-  snake.shift();
-  snake.push(new_cell);
-  draw_snake();
+  if (new_cell[0]>=board_size || new_cell[0]<0 ||
+    new_cell[1]>=board_size || new_cell[1]<0 ||
+    board[new_cell[0]][new_cell[1]]==cell_types['snake'])
+  {
+    game_over();
+    return;
+  }
+  if(board[new_cell[0]][new_cell[1]]!=cell_types['food'])
+  {
+    snake.shift();
+    snake.push(new_cell);
+    draw_snake();
+  }
+  else{
+    snake.push(new_cell);
+    draw_snake();
+    random_food();
+  }
 }
 
 function handle_arrow(e){
-  console.log(e.key)
   switch(e.key){
     case 'ArrowUp':
       direction = 'up'
@@ -81,12 +100,16 @@ let board = init_board;
 let snake = init_snake;
 let direction = 'right';
 let game_interval;
+let lost;
+
+// use ? https://v2.grommet.io/starter
 
 function App() {
   const [game_board, set_game_board] = useState(draw_board());
 
   function handle_start(e){
     e.currentTarget.disabled = true;
+    lost = false;
     draw_snake();
     random_food();
     set_game_board(draw_board());
@@ -105,6 +128,7 @@ function App() {
       <div className='container'>
         {game_board}
       </div>
+      {lost && <div className='game_over_overlay'>Game Over</div>}
       <button type="button" onClick={handle_start}>Start</button>
     </>
   )
